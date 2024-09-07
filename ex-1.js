2a4545a26669f3d016363ec33e9215ccb6288a63
Kanompang <suparadajk21@gmail.com>
2024-09-07 22:08:49 +0700

    BP5-Kanompang

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,10 @@
-let isOver18;
-let hasCriminalBlacklist;
-
-let isAllow;
-
+// Exercise #1: Event Conditions
 // Start coding here
+let isOver18 = true;
+let hasCriminalBlacklist = true;
+let isAllow = true;
 
+isOver18 = !isOver18;
+hasCriminalBlacklist =!hasCriminalBlacklist
+isAllow = isOver18&&hasCriminalBlacklist;
 console.log(isAllow);
